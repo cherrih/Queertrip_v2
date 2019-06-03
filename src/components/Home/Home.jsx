422d00aeb210062426c4e04e1fea6b0ca737fe7a
cherrih <cherri.hartigan@gmail.com>
2019-06-03 13:56:27 -0700

    Add title and search bar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Header from './Header.jsx';
-import Poster from './Poster.jsx';
-import Cities from './Cities.jsx';
 
 const Home = (props) => {
   return (
-    <div>
-      <Header />
-      <Poster />
-      <Cities />
-    </div>
+    <>
+      <section id="logo"></section>
+      <h1 className="home-title">Helping queerdos <br/> travel queerer.</h1>
+      <div className="home-search">
+        <div>I'm traveling to...</div>
+        <button className="home-button">GO</button>
+      </div>
+    </>
   );
 };
 
