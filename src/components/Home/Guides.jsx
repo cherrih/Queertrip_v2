89614187ecf2087a72d3a66314bbfc6bd873cc3d
cherrih <cherri.hartigan@gmail.com>
2019-06-03 16:03:08 -0700

    Add NY guide

--- src/components/Home/Guides.jsx
+++ src/components/Home/Guides.jsx
@@ -4,6 +4,10 @@ const Guides = (props) => {
   return (
     <div>
       <h2>Guides</h2>
+      <div className="guide-container">
+        <img src="/public/images/ny.jpeg"/>
+        <h3>Queer guide to NYC</h3>
+      </div>
     </div>
   );
 }
